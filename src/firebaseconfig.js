e9819c748a82caf305ea67e673eaedf3705474be
med4it <mohammed.aithaddou@gmail.com>
2019-10-12 20:18:50 +0100

    firebase config file updated

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCvch-v0jF0ggIHN1QqeDMxQ_UI-DhfTJY",
-  authDomain: "msiosm.firebaseapp.com",
-  databaseURL: "https://msiosm.firebaseio.com",
-  projectId: "msiosm",
-  storageBucket: "msiosm.appspot.com",
-  messagingSenderId: "318782896244",
-  appId: "1:318782896244:web:c40fdcdaba136a81"
-};
+  apiKey: "",
+  authDomain: """#############"".firebaseapp.com",
+  databaseURL: "https://""#############"".firebaseio.com",
+  projectId: """#############""",
+  storageBucket: """#############"".appspot.com",
+  messagingSenderId: "",
+  appId: "";
+}
